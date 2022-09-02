11f7935f9a3a0bed6bf9201baa25307c1e5da9d0
sukantahui <huisukanta77@gmail.com>
2022-09-02 11:57:57 +0530

    first Commit

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,47 @@
+const db =require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+    // Create a Tutorial
+    const tutorial = {
+      title: req.body.title,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false
+    };
+    // Save Tutorial in the database
+    Tutorial.create(tutorial)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Tutorial."
+        });
+      });
+  };
+
+  //fetching 
+  exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+    Tutorial.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
+
