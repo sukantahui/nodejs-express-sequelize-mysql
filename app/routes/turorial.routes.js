581a786fc7351c2a939477dd62ea660e993b16fc
sukantahui <huisukanta77@gmail.com>
2022-09-02 13:30:39 +0530

    API created for Tutorial

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -10,14 +10,14 @@ module.exports = app => {
     // Retrieve all Tutorials
     router.get("/", tutorials.findAll);
     // // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
+    router.get("/published", tutorials.findAllPublished);
     // // Retrieve a single Tutorial with id
     router.get("/:id", tutorials.findOne);
     // // Update a Tutorial with id
-    // router.put("/:id", tutorials.update);
+    router.put("/:id", tutorials.update);
     // // Delete a Tutorial with id
-    // router.delete("/:id", tutorials.delete);
+    router.delete("/:id", tutorials.delete);
     // // Delete all Tutorials
-    // router.delete("/", tutorials.deleteAll);
+    router.delete("/", tutorials.deleteAll);
     app.use('/api/tutorials', router);
   };
